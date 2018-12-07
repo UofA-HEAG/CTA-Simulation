a3e82d2c0ad1b6c54897a23b6ee15ee81b4e0774
Harry Poulter <harry.poulter@adelaide.edu.au>
2018-12-07 11:55:40 +1030

    Add 5 SST data card and setup script
    This data card is used to steer CORSIKA for a simple 5 SST array. The
    accompanying setup script symbolically links this file in the place
    CORSIKA expects it.

---
+++ data/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Setup all non-standard files for CTA CORSIKA/sim_telarray
+
+CTA_PATH=${HOME}/cta/CTA_MC
+
+# Get current directory this script is located in, from:
+# https://stackoverflow.com/questions/59895/
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+WORK_PATH="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+
+# Link files
+ln -sf ${WORK_PATH}/INPUTS_CTA_PROD4-Paranal-5-SST-template-20deg ${CTA_PATH}/corsika-run
