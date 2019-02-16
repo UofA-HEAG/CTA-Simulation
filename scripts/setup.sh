4033773fd9692dfb622e27e86ba3a47fb3068400
Harry Poulter <harry.poulter@adelaide.edu.au>
2019-02-16 22:07:22 +1030

    Add option to create links to .job files in /fast
    Just for convenience. That way users aren't faffing about with large
    path names in calls to sbatch from /fast.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,3 +6,25 @@ if [ ! -d ${FAST_PARTITION}/Data ]; then
   mv ${CTA_PATH}/Data ${FAST_PARTITION}/
   ln -s ${FAST_PARTITION}/Data ${CTA_PATH}
 fi
+
+# Ask if users want to have links to .job files in their /fast partition
+# This is just a quality-of-life option so calls to sbatch in /fast don't need to specify the full path to the .job file
+while true; do
+  read -p "Would you like to create .job file links in ${FAST_PARTITION}? [y/N] " reply
+
+  if [ -z $reply ]; then
+    reply=N
+  fi
+
+  case "$reply" in
+    Y*|y*)
+      for job in $( ls ${REPO_PATH}/scripts/batch/*.job ); do
+        ln -sf $( readlink -f $job ) ${FAST_PARTITION}/$( basename $job )
+      done
+      ;;
+    N*|n*)
+      break ;;
+  esac
+
+  printf "Invalid reply '%s'\n" $reply
+done
